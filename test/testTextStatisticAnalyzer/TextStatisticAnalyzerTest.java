d40630694cb76d4c632b9615dee4ed0f7ebf2743
DeathgodSS <slavadeathgod@gmail.com>
2020-05-28 23:12:13 +0300

    Empty string check Small fix

--- test/testTextStatisticAnalyzer/TextStatisticAnalyzerTest.java
+++ test/testTextStatisticAnalyzer/TextStatisticAnalyzerTest.java
@@ -1,17 +1,18 @@
 package testTextStatisticAnalyzer;
 
-import textStatisticAnalyzer.TextStatisticAnalyzer;
+import textStatisticAnalyzer.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
-public class TextStatisticAnalyzerTest {
 
+public class TextStatisticAnalyzerTest {
     String text;
     String expected;
+
     TextStatisticAnalyzer textStatisticAnalyzer;
+    TextStatisticAnalyzer textStatisticAnalyzerNegativeCase;
 
     @Before
     public void setUp() {
@@ -23,9 +24,6 @@ public class TextStatisticAnalyzerTest {
                 "Their names are Tim and Bim. This is her granny. " +
                 "Her name is Suicide. " +
                 "This is her grandad. His name is Tom.";
-
-        textStatisticAnalyzer = new TextStatisticAnalyzer(text);
-
         expected = "A:  a 5\n" + "\tand 3\n" + "\tare 4\n" +
                 "B:  big 1\n" + "\tbim 1\n" + "\tbrothers 2\n" +
                 "D:  dad 2\n" +
@@ -36,10 +34,13 @@ public class TextStatisticAnalyzerTest {
                 "L:  liz 1\n" + "\tlolly 1\n" +
                 "M:  mum 2\n" +
                 "N:  name 4\n" + "\tnames 2\n" +
-                "P:  pam 1\n" + "R:  rose 2\n" +
+                "P:  pam 1\n" +
+                "R:  rose 2\n" +
                 "S:  sam 1\n" + "\tshe 1\n" + "\tshe's 1\n" + "\tsisters 2\n" + "\tsuicide 1\n" +
                 "T:  ten 1\n" + "\ttheir 2\n" + "\tthese 2\n" + "\tthis 5\n" + "\ttim 1\n" + "\ttom 1\n" + "\ttwo 2\n";
 
+        textStatisticAnalyzer = new TextStatisticAnalyzer(text);
+        textStatisticAnalyzerNegativeCase = new TextStatisticAnalyzer("");
     }
 
     @After
@@ -49,6 +50,12 @@ public class TextStatisticAnalyzerTest {
     @Test
     public void receiveAnalise() {
         String actual = textStatisticAnalyzer.receiveAnalise();
+        assertEquals(expected, actual);
+    }
+
+    @Test (expected = EmptyStringException.class)
+    public void receiveAnaliseFromEmptyString() {
+        String actual = textStatisticAnalyzerNegativeCase.receiveAnalise();
         assertEquals(expected, actual);
     }
 }
